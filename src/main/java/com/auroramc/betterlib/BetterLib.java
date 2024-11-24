599183baf601e84414cdef12792400502b7c7233
simondumalski <simon@dumalski.com>
2024-11-24 15:26:24 -0600

    chore: init

---
+++ src/main/java/com/auroramc/betterlib/BetterLib.java
@@ -0,0 +1,17 @@
+package com.auroramc.betterlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+    
+}
