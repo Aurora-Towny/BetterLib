021dc4b6c14b0d995e9291f7b1a9baacac18e230
simondumalski <simon@dumalski.com>
2024-11-24 15:57:23 -0600

    style: collapse onEnable and onDisable

--- src/main/java/com/auroramc/betterlib/BetterLib.java
+++ src/main/java/com/auroramc/betterlib/BetterLib.java
@@ -5,13 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class BetterLib extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-
-    }
+    public void onEnable() {}
 
     @Override
-    public void onDisable() {
-
-    }
+    public void onDisable() {}
     
 }
